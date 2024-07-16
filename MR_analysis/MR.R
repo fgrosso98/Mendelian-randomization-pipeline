165d870394fd16848016e0dfb9fd71bf8850162c
fgrosso98 <148697309+fgrosso98@users.noreply.github.com>
2024-07-16 12:31:21 +0200

    MR.R

--- MR_analysis/MR.R
+++ MR_analysis/MR.R
@@ -1,3 +1,6 @@
+################################################################################
+### Libraries
+################################################################################
 library(dplyr)
 require(TwoSampleMR)
 require(ieugwasr)
@@ -5,23 +8,25 @@ require(ggplot2)
 require(MRPRESSO)
 library(officer)
 source("/home/students/federica.grosso/nas/MR_PRESSO_plots.R")
-# Leggi il percorso del file dalla linea di comando
+
+# Read path from command line
 args <- commandArgs(trailingOnly = TRUE)
 file_path_exposure <- args[1]
 file_path_outcome <- args[2]
 
-# RICAVA NOME EXPOSURE
-nome_fileE <- basename(file_path_exposure)
-nome_file_senza_ext <- tools::file_path_sans_ext(nome_fileE)
+# File name exposure
+file_nameE <- basename(file_path_exposure)
+file_name_without_ext <- tools::file_path_sans_ext(file_nameE)
 # Divide il nome del file basandosi sul carattere "_"
-parti_nome <- strsplit(nome_file_senza_ext, "_")[[1]]
-nome_exposure <- paste(parti_nome[2])
+name_parts <- strsplit(file_name_without_ext, "_")[[1]]
+exposure_name <- paste(name_parts[2])
 
-# RICAVA NOME OUTCOME
-nome_fileO <- basename(file_path_outcome)
-parti_outcome <- strsplit(nome_fileO, split = "_")[[1]]
-nome_outcome <- parti_outcome[1]
+# File name outcome
+file_nameO <- basename(file_path_outcome)
+outcome_parts <- strsplit(file_nameO, split = "_")[[1]]
+outcome_name <- outcome_parts[1]
 
+# Upload exposure file and outcome file
 dataE<-read.csv(file_path_exposure, header=T)
 suppressWarnings({
   dataO<-read_outcome_data(file_path_outcome, 
@@ -38,91 +43,81 @@ suppressWarnings({
   dataO$outcome <- "CAD"
 })
 
+################################################################################
+### MENDELIAN RANDOMIZATION ANALYSIS
+################################################################################
+
 # HARMONIZE
-output_file <- paste0("/home/students/federica.grosso/nas/microbiome/Results_CAD/Harm_data_E_", nome_exposure, "_O_", nome_outcome, ".txt")
+output_file <- paste0("/home/students/federica.grosso/nas/microbiome/Results_CAD/Harm_data_E_", exposure_name, "_O_", outcome_name, ".txt")
 dat <- harmonise_data(dataE, dataO, action = 2)
 write.table(dat, file = output_file, sep = "\t", quote=F)
 
 # MR RESULTS
-output_file <- paste0("/home/students/federica.grosso/nas/microbiome/Results_CAD/MR_E_", nome_exposure, "_O_", nome_outcome, ".csv")
+output_file <- paste0("/home/students/federica.grosso/nas/microbiome/Results_CAD/MR_E_", exposure_name, "_O_", outcome_name, ".csv")
 mr_results <- mr(dat, method_list = c("mr_egger_regression", "mr_ivw","mr_wald_ratio","mr_weighted_median"))
 write.csv(mr_results, file = output_file, quote=F, row.names = F)
-# wald solo quando ho solo uno SNP, il p di significatività è 0.05
+# wald with one SNP, significant p-value < 0.05
 
-# Sensitivity analysis
-# Crea un nome di file unico per il file di output (puoi modificare questa parte come preferisci)
-output_file <- paste0("/home/students/federica.grosso/nas/microbiome/Results_CAD/Het_E_", nome_exposure, "_O_", nome_outcome, ".txt")
+################################################################################
+### SENSITIVITY ANALYSIS
+################################################################################
+
+# Heterogeneity test
+output_file <- paste0("/home/students/federica.grosso/nas/microbiome/Results_CAD/Het_E_", exposure_name, "_O_", outcome_name, ".txt")
 het<-mr_heterogeneity(dat)
 write.table(het, file = output_file, sep = "\t", quote=F)
 
-#cat("\n\n--- Pleiotropy ---\n\n", file = output_file, append = TRUE)
-output_file <- paste0("/home/students/federica.grosso/nas/microbiome/Results_CAD/Ple_E_", nome_exposure, "_O_", nome_outcome, ".txt")
+# Pleiotropy test
+output_file <- paste0("/home/students/federica.grosso/nas/microbiome/Results_CAD/Ple_E_", exposure_name, "_O_", outcome_name, ".txt")
 ple <- mr_pleiotropy_test(dat)
 write.table(ple, file = output_file, sep = "\t", quote=F)
 
-output_file <- paste0("/home/students/federica.grosso/nas/microbiome/Results_CAD/SINGLESNP_E_", nome_exposure, "_O_", nome_outcome, ".txt")
+# Single SNP analysis
+output_file <- paste0("/home/students/federica.grosso/nas/microbiome/Results_CAD/SINGLESNP_E_", exposure_name, "_O_", outcome_name, ".txt")
 res_single <- mr_singlesnp(dat)
 write.table(res_single, file = output_file, sep = "\t", quote=F)
 
-output_file <- paste0("/home/students/federica.grosso/nas/microbiome/Results_CAD/LOO_E_", nome_exposure, "_O_", nome_outcome, ".txt")
+# Leave-one-out analysis
+output_file <- paste0("/home/students/federica.grosso/nas/microbiome/Results_CAD/LOO_E_", exposure_name, "_O_", outcome_name, ".txt")
 res_loo <- mr_leaveoneout(dat)
 write.table(res_loo, file = output_file, sep = "\t", quote=F)
 
 # MR Steiger directionality test
 # output_file <- paste0("/home/students/federica.grosso/nas/MRresults2/BMI/dir_E_", nome_proteina, "_O_", nome_outcome, ".txt")
-#cat("\n\n--- Directionality ---\n\n", file = output_file, append = TRUE)
 #dir <- directionality_test(dat) # non posso farlo perchè non ho sample size
 #write.table(dir, file = output_file, sep = "\t", quote=F, append = TRUE)
 
-# PLOTS
+################################################################################
+### PLOTS
+################################################################################
 
 ## Scatter plot
-output_file <- paste0("/home/students/federica.grosso/nas/microbiome/Results_CAD/plots_E_", nome_exposure, "_O_", nome_outcome, ".pdf")
+output_file <- paste0("/home/students/federica.grosso/nas/microbiome/Results_CAD/plots_E_", exposure_name, "_O_", outcome_name, ".pdf")
 pdf(output_file)
 
 p1<-mr_scatter_plot(mr_results,dat)
-#title("Scatter Plot")
 print(p1)
 
 ## Forest plot
-
 p2 <- mr_forest_plot(res_single)
-#title("Forest Plot")
 print(p2)
 
 ## Leave-one-out plot
-
 p3 <- mr_leaveoneout_plot(res_loo)
-#title("Leave-One-Out Plot")
 print(p3)
 
 ## Funnel plot
-## Asymmetry in a funnel plot is useful for gauging the reliability of a particular MR analysis
-
 p4 <- mr_funnel_plot(res_single)
-#title("Funnel Plot")
 print(p4)
 
 dev.off()
 
-ivw_results <- subset(mr_results, method == "Inverse variance weighted")
+################################################################################
+### MR EGGER
+################################################################################
 
-if (length(ivw_results$pval) > 0 && !is.na(ivw_results$pval) && ivw_results$pval<0.05)
-{
-  output_file <- paste0("/home/students/federica.grosso/nas/microbiome/Results_CAD/Significative/YES_E_", nome_exposure, "_O_", nome_outcome, ".txt")
-  # Apri il file in modalità scrittura
-  file_conn <- file(output_file, "w")
-  # Scrivi il messaggio sul file
-  cat(paste("The ", nome_exposure, " is significative with p-value of IVW of ", subset(mr_results, method == "Inverse variance weighted")$pval), file = file_conn)
-  # Chiudi il file
-  close(file_conn)
-}
-
-
-if (length(ivw_results$pval) > 0 && !is.na(ivw_results$pval) && ivw_results$pval<0.05 && !is.na(ple$pval) && ple$pval<0.05)
-{
-  output_file <- paste0("/home/students/federica.grosso/nas/microbiome/Results_CAD/MR_PRESSO/MR_PRESSO_E_", nome_exposure, "_O_", nome_outcome, ".txt")
-  mr_presso <- mr_presso(BetaOutcome="beta.outcome", 
+output_file <- paste0("/home/students/federica.grosso/nas/microbiome/Results_CAD/MR_PRESSO/MR_PRESSO_E_", exposure_name, "_O_", outcome_name, ".txt")
+mr_presso <- mr_presso(BetaOutcome="beta.outcome", 
                          BetaExposure="beta.exposure", 
                          SdOutcome="se.outcome", 
                          SdExposure="se.exposure", 
@@ -131,35 +126,35 @@ if (length(ivw_results$pval) > 0 && !is.na(ivw_results$pval) && ivw_results$pval
                          DISTORTIONtest = TRUE, 
                          SignifThreshold = 0.05, 
                          NbDistribution = 1000, seed = 1)
-  output_file <- paste0("/home/students/federica.grosso/nas/microbiome/Results_CAD/MR_PRESSO/MR_PRESSO_E_", nome_exposure, "_O_", nome_outcome, ".docx")
-  doc <- read_docx()
+output_file <- paste0("/home/students/federica.grosso/nas/microbiome/Results_CAD/MR_PRESSO/MR_PRESSO_E_", exposure_name, "_O_", outcome_name, ".docx")
+
+# Save results in a file word
+doc <- read_docx()
+doc <- doc %>%
+  body_add_par("MR Presso results:", style = "heading 1")
   
-  # Aggiungi il contenuto del file RDS al documento
-  doc <- doc %>%
-    body_add_par("MR Presso results:", style = "heading 1")
-  
-  # Aggiungi i risultati di 'Main MR results'
+# 'Main MR results'
   doc <- doc %>%
     body_add_par("Main MR results:", style = "heading 2") %>%
     body_add(mr_presso$`Main MR results`)
   
-  # Aggiungi i risultati di 'MR-PRESSO results'
+# 'MR-PRESSO results'
   doc <- doc %>%
     body_add_par("MR-PRESSO results:", style = "heading 2") %>%
     body_add_par("Outlier test:", style = "heading 3") %>%
     body_add(mr_presso$`MR-PRESSO results`$`Outlier Test`)
-  
-  # Aggiungi i risultati di 'MR-PRESSO results'
+
+# Global test RSS
   doc <- doc %>%
     body_add_par("Global test - RSSobs:", style = "heading 3") %>%
     body_add(mr_presso$`MR-PRESSO results`$`Global Test`$RSSobs)
   
-  # Aggiungi i risultati di 'MR-PRESSO results'
+# Global test p-value
   doc <- doc %>%
     body_add_par("Global test - Pvalue:", style = "heading 3") %>%
     body_add(mr_presso$`MR-PRESSO results`$`Global Test`$Pvalue)
   
-  # Aggiungi i risultati di 'MR-PRESSO results'
+#  Distortion test
   doc <- doc %>%
     body_add_par("Distortion Test - Outlier indices:", style = "heading 3") %>%
     body_add(mr_presso$`MR-PRESSO results`$`Distortion Test`$`Outliers Indices`)
@@ -171,25 +166,22 @@ if (length(ivw_results$pval) > 0 && !is.na(ivw_results$pval) && ivw_results$pval
   doc <- doc %>%
     body_add_par("Distortion Test - Pval:", style = "heading 3") %>%
     body_add(mr_presso$`MR-PRESSO results`$`Distortion Test`$Pvalue)
-  # Salva il documento Word
-  print(doc, target = output_file)
-  
-  
-  outliers_removed <- mr_presso$`MR-PRESSO results`$`Distortion Test`$`Outliers Indices`
-  # Aggiungi una nuova colonna a `dat` per indicare se un SNP è un outlier
-  dat$outlier_status <-  ifelse(seq_along(dat$SNP) %in% outliers_removed, "Removed", "Kept")
-  
-  # Filtra il dataset per includere solo gli SNP mantenuti
-  dat_kept <- subset(dat, outlier_status == "Kept")
-  dat_removed <- subset(dat, outlier_status == "Removed")
-  mr_results_kept <- mr(dat_kept, method_list = c("mr_egger_regression", "mr_ivw","mr_wald_ratio","mr_weighted_median"))
-  output_file <- paste0("/home/students/federica.grosso/nas/microbiome/Results_CAD/MR_PRESSO/MR_PRESSO_E_", nome_exposure, "_O_", nome_outcome, ".pdf")
-  pdf(output_file)
-  p1 <- mr_scatter_plot_col(mr_results,dat,dat_kept,dat_removed)
-  print(p1)
-  p2 <- mr_scatter_plot_IVW(mr_results,mr_results_kept,dat,dat_kept,dat_removed)
-  print(p2)
-  dev.off()
-}
+print(doc, target = output_file)
+
+# Scatter plot of MR-PRESSO (res dots for removed variants)
+outliers_removed <- mr_presso$`MR-PRESSO results`$`Distortion Test`$`Outliers Indices`
+dat$outlier_status <-  ifelse(seq_along(dat$SNP) %in% outliers_removed, "Removed", "Kept")
+
+dat_kept <- subset(dat, outlier_status == "Kept")
+dat_removed <- subset(dat, outlier_status == "Removed")
+mr_results_kept <- mr(dat_kept, method_list = c("mr_egger_regression", "mr_ivw","mr_wald_ratio","mr_weighted_median"))
+output_file <- paste0("/home/students/federica.grosso/nas/microbiome/Results_CAD/MR_PRESSO/MR_PRESSO_E_", exposure_name, "_O_", outcome_name, ".pdf")
+
+pdf(output_file)
+p1 <- mr_scatter_plot_col(mr_results,dat,dat_kept,dat_removed)
+print(p1)
+p2 <- mr_scatter_plot_IVW(mr_results,mr_results_kept,dat,dat_kept,dat_removed)
+print(p2)
+dev.off()
 
 
